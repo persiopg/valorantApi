28294501e51d8fec22c5ad239361ed5e51f980ee
persio de paula godoy <40552268+persiopg@users.noreply.github.com>
2022-05-25 19:50:30 -0300

    geracao de tabela ok

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
-let data = fazGet("https://br.api.riotgames.com/val/content/v1/contents?api_key=RGAPI-49971981-b081-41e6-a016-f60750363b6f");
-let usuario = JSON.parse(data);
+let data = fazGet("https://br.api.riotgames.com/val/content/v1/contents?api_key=RGAPI-39f88746-af19-45d5-ba01-e65a57a42825");
+var usuario = JSON.parse(data);
+const localizedNamesSuport = [
+    "ar-AE",
+    "de-DE",
+    "en-US",
+    "es-ES",
+    "es-MX",
+    "fr-FR",
+    "id-ID",
+    "it-IT",
+    "ja-JP",
+    "ko-KR",
+    "pl-PL",
+    "pt-BR",
+    "ru-RU",
+    "th-TH",
+    "tr-TR",
+    "vi-VN",
+    "zh-CN",
+    "zh-TW"
+]
 
 function fazGet(url){
     let req = new XMLHttpRequest();
@@ -8,39 +28,101 @@ function fazGet(url){
     return req.responseText;
 }
 
-function criaLinha(usuario){
-    console.log(usuario.name)
-    linha = document.createElement("tr");
-    tdId = document.createElement("td");
-    tdMapName = document.createElement("td");    
-    tdId.innerHTML = usuario.id;
-    tdMapName.innerHTML = usuario.name;
-    linha.appendChild(tdId)
-    linha.appendChild(tdMapName);
-    return linha;
+
+/*function test(usuario){
+    let x = 0;
+    let y=0;
+    while(x < usuario.length){
+        console.log("test "+ usuario[x].localizedNames["pt-BR"]);
+        while(y<localizedNamesSuport.length){
+            console.log("test: "+ localizedNamesSuport[y] +": "+ usuario[x].localizedNames[localizedNamesSuport[y]]);
+            y++
+        }
+        y=0;
+        x++;
+    }
+}*/
+
+
+function criaLinha(api){
+  
+    let linha = document.createElement("tr");
+    let tdName = document.createElement("td");
+    let tdLInhaLocalized = document.createElement("td");
+    let t=0;
+
+    if(api.name == "Null UI Data!"){
+        return linha;
+    }else{
+
+        tdName.innerHTML = `<a href="${api.name}"> Name: ${api.name} </a>`;
+        linha.appendChild(tdName);
+        tdLInhaLocalized.innerHTML = `<table id="${api.assetName}" class="tabelagerada">`;
+        linha.appendChild(tdLInhaLocalized);        
+        return linha;
+    }
 }
-function gerarTabela(usuario){
-    Array.from(usuario).forEach(element => {
+function GerarLocalizedNames(api){
+    
+    Array.from(api).forEach(element => {
+            if(element.name == "Null UI Data!"){}
+            else{
+            let id = element.assetName
+            let tabela = document.getElementById(`${id}`);
+            let t=0
+            while(t<localizedNamesSuport.length){
+                
+                let linha = criaTabelaLocalized(element.localizedNames[localizedNamesSuport[t]],t);
+                
+                tabela.appendChild(linha); 
+                t++
+            }
+        }
+    });
+}
+
+function criaTabelaLocalized(api,x){
+
+    let linha = document.createElement("tr");
+    let tdName = document.createElement("td");
+    let tdLInhaLocalized = document.createElement("td");
+
+    tdName.innerHTML = localizedNamesSuport[x]
+    tdLInhaLocalized.innerHTML = api;
+    linha.appendChild(tdName);
+    linha.appendChild(tdLInhaLocalized);
+
+    return linha;
+
+
+}
+function gerarTabela(api, tabela){
+    Array.from(api).forEach(element => {
         let linha = criaLinha(element);
-        console.log(linha);
-        tabela.appendChild(linha);
-    })
+        tabela.appendChild(linha);        
+    });
+    GerarLocalizedNames(api)
 }
 
 function carregaChar(){
     
-    gerarTabela(usuario.characters);
+    document.getElementById("tabelamaps").style.visibility = "hidden";
+    document.getElementById("tabelachar").style.visibility = "visible";
 }
 
 function carregaMaps(){
 
-    gerarTabela(usuario.maps);
+    document.getElementById("tabelachar").style.visibility = "hidden";
+    document.getElementById("tabelamaps").style.visibility = "visible";
 }
 
+
 function main(){
-    
-    let tabela = document.getElementById("tabela");
     console.log(usuario)
+    let tabelamaps = document.getElementById("tabelamaps");
+    let tabelachar = document.getElementById("tabelachar");
+    gerarTabela(usuario.maps, tabelamaps);
+    gerarTabela(usuario.characters, tabelachar);
     
     
 }
